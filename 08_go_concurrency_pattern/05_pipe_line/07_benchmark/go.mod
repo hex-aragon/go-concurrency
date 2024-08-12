f2089d88f4064c707aea228b97315faf241d8365
hex-aragon <0xaragon90@gmail.com>
2024-08-13 01:19:22 +0900

    ADD: fanin, fanout

---
+++ 08_go_concurrency_pattern/05_pipe_line/07_benchmark/go.mod
@@ -0,0 +1,3 @@
+module benchmark_pipe
+
+go 1.22.3
---
+++ 08_go_concurrency_pattern/05_pipe_line/07_benchmark/test.sh
@@ -0,0 +1,11 @@
+go test -bench=.
+
+
+# goos: linux
+# goarch: amd64
+# pkg: benchmark_pipe
+# cpu: 13th Gen Intel(R) Core(TM) i5-13500H
+# BenchmarkGeneric-16      1011627              1253 ns/op
+# BenchmarkTyped-16        1658298               614.5 ns/op
+# PASS
+# ok      benchmark_pipe  3.800s
